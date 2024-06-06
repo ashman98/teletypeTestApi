6d9d89f6829d1d5cafb4d86d74836c80396c8e7a
ashman98 <manukyan15199815@gmail.com>
2024-06-06 04:45:55 +0400

    f

--- config/params.php
+++ config/params.php
@@ -1,9 +1,6 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@example.com',
-    'senderEmail' => 'noreply@example.com',
-    'senderName' => 'Example.com mailer',
     'teletypeApiUrl' => 'https://api.teletype.app/public/api/v1/',
     'teletypeApiKey' => 'GcElU5FXZkgWzYzwqLEk1YUQob8hpbFjZvbAvCxTGnavl842elPND1zrJRON4YxI',
 ];
